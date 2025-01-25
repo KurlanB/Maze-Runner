32520c2b2e5bf5ce3fede4dfeb78e591dd8b73fd
KurlanB <kurlan.bee@gmail.com>
2025-01-24 20:31:18 -0500

    Completed Walking Skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Coordinate.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Coordinate.java
@@ -1,5 +1,23 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Coordinate {
-    
+    private int cordX;
+    private int cordY;
+
+    public Coordinate(int x, int y) {
+        cordX = x;
+        cordY = y;
+    }
+
+    public int getX() {
+        return cordX;
+    }
+
+    public int getY() {
+        return cordY;
+    }
+
+    public void move(Orientation orientation){
+        
+    }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRule.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandRule.java
@@ -3,6 +3,7 @@ package ca.mcmaster.se2aa4.mazerunner;
 public class RightHandRule implements MazeRunner{
     
     public String escapeMaze(Maze maze) {
-        return null;
+        
     }
+
 }
