3aca64742a2dfcdf6009ff5d00521d85cd96b348
KurlanB <kurlan.bee@gmail.com>
2025-04-04 21:24:51 -0400

    Added all test cases and all passed

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/CoordinateTest.java
@@ -0,0 +1,49 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CoordinateTest {
+    private Coordinate coordinate;
+
+    @BeforeEach
+    public void initializeCoordinate() {
+        coordinate = new Coordinate(5, 10);    
+    }
+
+    @Test
+    public void testGetOrientation() {
+        assertEquals(Orientation.EAST, coordinate.getOrientation());
+    }
+
+    @Test
+    public void testGetX() {
+        assertEquals(5, coordinate.getX());
+    }
+
+    @Test
+    public void testGetY() {
+        assertEquals(10, coordinate.getY());
+    }
+
+    @Test
+    public void testMoveForward(){
+        coordinate.moveForward();
+        assertEquals(6, coordinate.getX());
+        assertEquals(10, coordinate.getY());
+    }
+
+    @Test
+    public void testTurnLeft(){
+        coordinate.turnLeft();
+        assertEquals(Orientation.NORTH, coordinate.getOrientation());
+    }
+
+    @Test
+    public void testTurnRight(){
+        coordinate.turnRight();
+        assertEquals(Orientation.SOUTH, coordinate.getOrientation());
+    }
+    
+}
---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/MazeTest.java
@@ -0,0 +1,48 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MazeTest {
+    private Maze maze;
+    private Maze maze2;
+
+    @BeforeEach
+    public void intializeMaze() throws Exception{
+        maze = new Maze("./examples/rectangle.maz.txt");
+        maze2 = new Maze("./examples/direct.maz.txt");
+    }
+
+    @Test
+    public void testMazeSize() {
+        int expectedLength = 21;
+        int expectedWidth = 51;
+        assertEquals(expectedLength, maze.getLength());
+        assertEquals(expectedWidth, maze.getWidth());
+    }
+
+    @Test
+    public void testMazeEntry() {
+        Coordinate entry = maze.getEntry();
+        assertEquals(0, entry.getX());
+        assertEquals(12, entry.getY());
+    }
+
+    @Test
+    public void testMazeExit() {
+        Coordinate exit = maze.getExit();
+        assertEquals(50, exit.getX());
+        assertEquals(10, exit.getY());
+    }
+
+    @Test
+    public void testCheckPath(){
+        boolean correctPath = maze2.checkPath("FRFFLFFFRFLFRFLFF");
+        assertTrue(correctPath);
+    }
+
+    
+}
